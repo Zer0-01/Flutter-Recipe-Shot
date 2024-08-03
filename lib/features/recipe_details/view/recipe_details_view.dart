76c47cb2bb306444af0502c63495025f24f2e7e1
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-08-03 14:00:43 +0800

    feat: add recipe details feature
    get recipe document from firebase
    display on view

--- lib/features/recipe_details/view/recipe_details_view.dart
+++ lib/features/recipe_details/view/recipe_details_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_shot/data/remote/response/api_status.dart';
+import 'package:flutter_recipe_shot/features/recipe_details/view_model/recipe_details_view_model.dart';
+import 'package:provider/provider.dart';
 
 class RecipeDetailsView extends StatefulWidget {
   static const String id = 'recipe_details_view';
@@ -11,11 +14,40 @@ class RecipeDetailsView extends StatefulWidget {
 }
 
 class _RecipeDetailsViewState extends State<RecipeDetailsView> {
+  RecipeDetailsViewModel vm = RecipeDetailsViewModel();
+  @override
+  void initState() {
+    super.initState();
+    vm.init(widget.recipeId);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      title: Text('Recipe Details ${widget.recipeId}'),
-    ));
+    return ChangeNotifierProvider<RecipeDetailsViewModel>(
+      create: (context) => vm,
+      child: Consumer<RecipeDetailsViewModel>(
+        builder: (context, vm, _) {
+          switch (vm.recipeResponse.status) {
+            case ApiStatus.LOADING:
+              return const CircularProgressIndicator();
+            case ApiStatus.ERROR:
+              return const Placeholder();
+            case ApiStatus.COMPLETED:
+              return Scaffold(
+                  appBar: AppBar(
+                    title: Text('Recipe Details ${widget.recipeId}'),
+                  ),
+                  body: Column(
+                    children: [
+                      Text('Recipe Name: ${vm.recipe.id}'),
+                      Text('Recipe Description: ${vm.recipe.description}'),
+                    ],
+                  ));
+            default:
+          }
+          return Container();
+        },
+      ),
+    );
   }
 }
