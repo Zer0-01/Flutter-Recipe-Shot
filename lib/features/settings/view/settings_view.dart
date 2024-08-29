85384b55191879c1374e92a9fca511fd5cf95711
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-08-29 21:57:31 +0800

    feat: Implement logout logic

--- lib/features/settings/view/settings_view.dart
+++ lib/features/settings/view/settings_view.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_shot/features/settings/view_model/settings_view_model.dart';
+import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
 
 class SettingsView extends StatefulWidget {
@@ -9,37 +13,66 @@ class SettingsView extends StatefulWidget {
 }
 
 class _SettingsViewState extends State<SettingsView> {
+  SettingsViewModel vm = SettingsViewModel();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: SettingsList(
-        sections: [
-          SettingsSection(
-            title: const Text('General'),
-            tiles: [
-              SettingsTile(
-                title: const Text('Logout'),
-                leading: const Icon(Icons.logout),
-                onPressed: (context) {},
-              ),
-              SettingsTile(
-                title: const Text('Privacy'),
-                description: const Text('Privacy policy'),
-                leading: const Icon(Icons.privacy_tip),
-                onPressed: (context) {},
-              ),
-              SettingsTile(
-                title: const Text('About'),
-                description: const Text('About the app'),
-                leading: const Icon(Icons.info),
-                onPressed: (context) {},
-              ),
-            ],
-          ),
-        ],
+      body: ChangeNotifierProvider<SettingsViewModel>(
+        create: (context) => vm,
+        child: SettingsList(
+          sections: [
+            SettingsSection(
+              title: const Text('General'),
+              tiles: [
+                SettingsTile(
+                  title: const Text('Logout'),
+                  leading: const Icon(Icons.logout),
+                  onPressed: (context) {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('Logout'),
+                            content:
+                                const Text('Are you sure you want to logout?'),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  log('Cancel');
+                                },
+                                child: Text('Cancel'),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  log('Logout');
+                                  vm.logout();
+                                },
+                                child: Text('Logout'),
+                              ),
+                            ],
+                          );
+                        });
+                  },
+                ),
+                SettingsTile(
+                  title: const Text('Privacy'),
+                  description: const Text('Privacy policy'),
+                  leading: const Icon(Icons.privacy_tip),
+                  onPressed: (context) {},
+                ),
+                SettingsTile(
+                  title: const Text('About'),
+                  description: const Text('About the app'),
+                  leading: const Icon(Icons.info),
+                  onPressed: (context) {},
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
