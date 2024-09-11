ce5d38e3a5456f806c343a18d7ba97e04fc4cb68
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-09-11 22:34:25 +0800

    feat: create card widget for feed

--- lib/features/feed/view/feed_view.dart
+++ lib/features/feed/view/feed_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_shot/features/feed/widgets/card_feed_widget.dart';
 
 class FeedView extends StatelessWidget {
   FeedView({super.key});
@@ -36,63 +37,10 @@ class FeedView extends StatelessWidget {
       itemCount: tweets.length,
       itemBuilder: (context, index) {
         var tweet = tweets[index];
-        return Card(
-          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(tweet.profileImageUrl),
-                    ),
-                    const SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(tweet.username,
-                            style: const TextStyle(fontWeight: FontWeight.bold)),
-                        Text(tweet.handle),
-                      ],
-                    ),
-                    const Spacer(),
-                    Text(tweet.timeAgo),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Text(tweet.content),
-                if (tweet.imageUrl.isNotEmpty)
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: Image.network(tweet.imageUrl),
-                  ),
-                const SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.comment),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.repeat),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.favorite_border),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.share),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
+        return CardFeedWidget(
+          username: tweet.username,
+          description: tweet.content,
+          imageUrl: tweet.imageUrl,
         );
       },
     );
