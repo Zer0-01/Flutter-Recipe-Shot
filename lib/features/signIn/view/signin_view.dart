92bf9226de609d4f1667f2c08f051dc8cc341dcd
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-11-20 22:44:26 +0800

    refactor: remove emthod that refer to context in viewmodel

--- lib/features/signIn/view/signin_view.dart
+++ lib/features/signIn/view/signin_view.dart
@@ -7,6 +7,7 @@ import 'package:flutter_recipe_shot/features/signup/view/signup_view.dart';
 import 'package:flutter_recipe_shot/res/colors/app_colors.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 
 class SigninView extends StatefulWidget {
   static const String id = 'signin_view';
@@ -27,178 +28,184 @@ class _SigninViewState extends State<SigninView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.darkGreenColor,
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: MediaQuery.of(context).size.height,
-            ),
-            child: Form(
-              key: _formKey,
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image(
-                      image: AssetImage(icLogo),
-                      height: 160,
-                      width: 146,
-                    ),
-                    Text(
-                      AppLocalizations.of(context)!.welcome_back,
-                      style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.normal,
-                          color: AppColors.whiteColor),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    SigninTextFormFieldWidget(
-                      controller: vm.emailController,
-                      obscureText: false,
-                      hintText: 'Email',
-                      leadingIcon: Icons.email,
-                      validator: (value) =>
-                          value!.isEmpty ? 'Enter an email' : null,
-                    ),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    SigninTextFormFieldWidget(
-                      controller: vm.passwordController,
-                      obscureText: _isObscure,
-                      leadingIcon: Icons.key,
-                      trailingIcon:
-                          _isObscure ? Icons.visibility_off : Icons.visibility,
-                      onPressed: () {
-                        setState(() {
-                          _isObscure = !_isObscure;
-                        });
-                      },
-                      hintText: 'Password',
-                      validator: (value) => value!.length < 6
-                          ? "Enter a password 6+ characters long"
-                          : null,
-                    ),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                    SignInButtonWidget(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          vm.signIn(context);
-                        }
-                      },
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 3,
-                          child: Container(
-                            height: 1,
-                            color: AppColors.WHITE,
-                            width: 200,
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 10.0),
-                            child: Text(
-                              AppLocalizations.of(context)!.or,
-                              style: const TextStyle(
-                                  fontSize: 12, color: AppColors.WHITE),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 3,
-                          child: Container(
-                            height: 1,
-                            color: AppColors.WHITE,
-                            width: 200,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Row(
+    return ChangeNotifierProvider(
+      create: (context) => vm,
+      child: Consumer<SigninViewModel>(builder: (context, vm, child) {
+        return Scaffold(
+          backgroundColor: AppColors.darkGreenColor,
+          body: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: MediaQuery.of(context).size.height,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: AppColors.WHITE),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: SvgPicture.asset(icFacebook),
-                          ),
+                        Image(
+                          image: AssetImage(icLogo),
+                          height: 160,
+                          width: 146,
+                        ),
+                        Text(
+                          AppLocalizations.of(context)!.welcome_back,
+                          style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.normal,
+                              color: AppColors.whiteColor),
                         ),
                         const SizedBox(
-                          width: 26,
+                          height: 40,
                         ),
-                        Container(
-                          height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: AppColors.WHITE),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: SvgPicture.asset(icGoogle),
-                          ),
+                        SigninTextFormFieldWidget(
+                          controller: vm.emailController,
+                          obscureText: false,
+                          hintText: 'Email',
+                          leadingIcon: Icons.email,
+                          validator: (value) =>
+                              value!.isEmpty ? 'Enter an email' : null,
                         ),
                         const SizedBox(
-                          width: 26,
+                          height: 32,
                         ),
-                        Container(
+                        SigninTextFormFieldWidget(
+                          controller: vm.passwordController,
+                          obscureText: _isObscure,
+                          leadingIcon: Icons.key,
+                          trailingIcon: _isObscure
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          onPressed: () {
+                            setState(() {
+                              _isObscure = !_isObscure;
+                            });
+                          },
+                          hintText: "Password",
+                          validator: (value) => value!.length < 6
+                              ? "Enter a password 6+ characters long"
+                              : null,
+                        ),
+                        const SizedBox(
+                          height: 32,
+                        ),
+                        SignInButtonWidget(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              vm.signIn(context);
+                            }
+                          },
+                        ),
+                        const SizedBox(
                           height: 50,
-                          width: 50,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: AppColors.WHITE),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: SvgPicture.asset(icApple),
-                          ),
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: Container(
+                                height: 1,
+                                color: AppColors.WHITE,
+                                width: 200,
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10.0),
+                                child: Text(
+                                  AppLocalizations.of(context)!.or,
+                                  style: const TextStyle(
+                                      fontSize: 12, color: AppColors.WHITE),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 3,
+                              child: Container(
+                                height: 1,
+                                color: AppColors.WHITE,
+                                width: 200,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: AppColors.WHITE),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: SvgPicture.asset(icFacebook),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 26,
+                            ),
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: AppColors.WHITE),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: SvgPicture.asset(icGoogle),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 26,
+                            ),
+                            Container(
+                              height: 50,
+                              width: 50,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: AppColors.WHITE),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: SvgPicture.asset(icApple),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 40),
+                        SignUpNavigatorWidget(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SignupView(),
+                                ));
+                          },
                         ),
                       ],
                     ),
-                    const SizedBox(height: 40),
-                    SignUpNavigatorWidget(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SignupView(),
-                            ));
-                      },
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
--- lib/features/signIn/view_model/signin_view_model.dart
+++ lib/features/signIn/view_model/signin_view_model.dart
@@ -1,18 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_shot/data/local/shared_preferences_helper.dart';
-import 'package:flutter_recipe_shot/features/main/view/main_view.dart';
-import 'package:flutter_recipe_shot/res/widgets/loading_dialog_widget.dart';
 
 class SigninViewModel extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  set isLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+
   Future<void> signIn(BuildContext context) async {
     try {
-      showLoading(context);
-
+      isLoading = true;
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
           email: emailController.text.trim(),
           password: passwordController.text.trim());
@@ -20,34 +26,11 @@ class SigninViewModel extends ChangeNotifier {
       String userUid = userCredential.user!.uid;
 
       await SharedPreferencesHelper.instance.setUserUid("USERUID", userUid);
-
-      dismissLoading(context);
-      showSnackBar('Login Success', context);
-
-      Navigator.pushReplacementNamed(context, MainView.id);
     } catch (e) {
-      print('Error ${e.toString()}');
-
-      dismissLoading(context);
-      showSnackBar('Invalid', context);
+      print(e.toString());
+      isLoading = false;
+    } finally {
+      isLoading = false;
     }
-  }
-
-  void showLoading(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return const LoadingDialogWidget();
-      },
-    );
-  }
-
-  void dismissLoading(BuildContext context) {
-    Navigator.pop(context);
-  }
-
-  void showSnackBar(String message, BuildContext context) {
-    final snackBar = SnackBar(content: Text(message));
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
