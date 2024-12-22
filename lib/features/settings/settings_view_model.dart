bc84e5bea929499d90dd77bcba320c91c354ccbc
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-12-22 22:38:14 +0800

    feat: clear the sharedpreferences when logout

--- lib/features/settings/settings_view_model.dart
+++ lib/features/settings/settings_view_model.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_recipe_shot/data/local/shared_preferences_helper.dart';
 
 class SettingsViewModel extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<void> logout() async {
     await _auth.signOut();
+    SharedPreferencesHelper.instance.clear();
   }
 }
