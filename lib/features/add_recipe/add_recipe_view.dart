cd752338976a0f0696b29930e69e5f952b0bbe59
Zer0-01 <anas.zulkifli2001@gmail.com>
2024-12-22 13:18:35 +0800

    feat: apply consumer

--- lib/features/add_recipe/add_recipe_view.dart
+++ lib/features/add_recipe/add_recipe_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_recipe_shot/features/add_recipe/add_recipe_view_model.dart';
 import 'package:flutter_recipe_shot/features/add_recipe/widgets/add_recipe_text_field_widget.dart';
 import 'package:flutter_recipe_shot/res/colors/app_colors.dart';
+import 'package:provider/provider.dart';
 
 class AddRecipeView extends StatefulWidget {
   const AddRecipeView({super.key});
@@ -22,60 +23,69 @@ class _AddRecipeViewState extends State<AddRecipeView> {
         backgroundColor: Colors.transparent,
         foregroundColor: AppColors.BASE_WHITE,
       ),
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Add Recipe",
-                style: TextStyle(
-                    color: AppColors.BASE_WHITE,
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 38.0),
-              AddRecipeTextFieldWidget(
-                  label: "Title", maxLines: 1, controller: vm.titleController),
-              const SizedBox(height: 22.0),
-              AddRecipeTextFieldWidget(
-                  label: "Description",
-                  maxLines: 2,
-                  controller: vm.descriptionController),
-              const SizedBox(height: 22.0),
-              AddRecipeTextFieldWidget(
-                  label: "Ingredients",
-                  maxLines: 3,
-                  controller: vm.ingredientsController),
-              const SizedBox(height: 22.0),
-              AddRecipeTextFieldWidget(
-                  label: "Instructions",
-                  maxLines: 5,
-                  controller: vm.instructionsController),
-              const SizedBox(height: 38.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.black,
-                  textStyle: const TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  minimumSize: Size(maxWidth, 56),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
+      body: ChangeNotifierProvider(
+        create: (context) => vm,
+        child: Consumer<AddRecipeViewModel>(
+          builder: (context, value, child) {
+            return SafeArea(
+                child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Add Recipe",
+                      style: TextStyle(
+                          color: AppColors.BASE_WHITE,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 38.0),
+                    AddRecipeTextFieldWidget(
+                        label: "Title",
+                        maxLines: 1,
+                        controller: vm.titleController),
+                    const SizedBox(height: 22.0),
+                    AddRecipeTextFieldWidget(
+                        label: "Description",
+                        maxLines: 2,
+                        controller: vm.descriptionController),
+                    const SizedBox(height: 22.0),
+                    AddRecipeTextFieldWidget(
+                        label: "Ingredients",
+                        maxLines: 3,
+                        controller: vm.ingredientsController),
+                    const SizedBox(height: 22.0),
+                    AddRecipeTextFieldWidget(
+                        label: "Instructions",
+                        maxLines: 5,
+                        controller: vm.instructionsController),
+                    const SizedBox(height: 38.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        textStyle: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        minimumSize: Size(maxWidth, 56),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text('Submit'),
+                    ),
+                    const SizedBox(height: 48.0),
+                  ],
                 ),
-                onPressed: () {},
-                child: const Text('Submit'),
               ),
-              const SizedBox(height: 48.0),
-            ],
-          ),
+            ));
+          },
         ),
-      )),
+      ),
     );
   }
 }
--- lib/models/add_recipe_model.dart
+++ lib/models/add_recipe_model.dart
@@ -1,9 +1,9 @@
 class AddRecipeModel {
-  String? id; // Optional, useful for databases that use unique IDs
+  String? id; 
   String title;
   String description;
-  List<String> ingredients; // Storing ingredients as a list
-  List<String> instructions; // Storing instructions as a list
+  List<String> ingredients; 
+  List<String> instructions; 
 
   AddRecipeModel({
     this.id,
@@ -13,27 +13,25 @@ class AddRecipeModel {
     required this.instructions,
   });
 
-  // Convert a Recipe object to a map (for database insertion)
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'title': title,
       'description': description,
-      'ingredients': ingredients.join(', '), // Save as a string if needed
-      'instructions': instructions.join('. '), // Save as a string if needed
+      'ingredients': ingredients.join(', '), 
+      'instructions': instructions.join('. '), 
     };
   }
 
-  // Create a Recipe object from a map (e.g., from the database)
   factory AddRecipeModel.fromMap(Map<String, dynamic> map) {
     return AddRecipeModel(
       id: map['id'],
       title: map['title'],
       description: map['description'],
       ingredients:
-          (map['ingredients'] as String).split(', '), // Parse string to list
+          (map['ingredients'] as String).split(', '), 
       instructions:
-          (map['instructions'] as String).split('. '), // Parse string to list
+          (map['instructions'] as String).split('. '), 
     );
   }
 }
